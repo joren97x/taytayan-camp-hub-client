0e0701981227820b168bf0cb9c47c0d68589df1f
joren97x <sumagangjoren@gmail.com>
2024-04-16 21:17:02 +0800

    added milktea homepage

---
+++ src/components/User/FeaturedProductCard.vue
@@ -0,0 +1,70 @@
+<script setup>
+
+    import { ref } from 'vue'
+    defineProps({ product: Object })
+
+    const dialog = ref(false)
+
+</script>
+
+<template>
+
+    <q-card class="col-3" flat>
+        <q-img src="https://cdn.quasar.dev/img/chicken-salad.jpg" />
+
+        <q-card-section>
+            <q-btn
+                fab
+                color="primary"
+                icon="add"
+                class="absolute"
+                @click="dialog = true"
+                style="top: 0; right: 12px; transform: translateY(-50%);"
+            />
+
+            <div class="row no-wrap items-center">
+                <div class="col text-h6 ellipsis">
+                    Cafe Basilico
+                </div>
+            </div>
+            <div class="text-caption text-grey">
+                Small plates, salads & sandwiches in an intimate setting.
+            </div>
+        </q-card-section>
+    </q-card>
+
+    <q-dialog v-model="dialog" full-width>
+        <q-card class="my-card q-px-md">
+            <q-item>
+                <q-item-section class="text-h6">
+                    Food description
+                </q-item-section>
+                <q-item-section side>
+                    <q-btn round icon="close" flat @click="dialog = false"></q-btn>
+                </q-item-section>
+            </q-item>
+            <div class="row q-col-gutter-md">
+                <div class="col-5">
+                    <q-img src="https://cdn.quasar.dev/img/chicken-salad.jpg" />
+                </div>
+                <div class="col-7">
+                    <p>
+                        <span class="text-h5">
+                            Cafe Basilico
+                        </span>
+                        <br>
+                        <span class="text-h6 text-weight-light">
+                            P65.00
+                        </span>
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, totam?
+                    </p>
+                    
+                </div>
+            </div>
+            
+        </q-card>
+    </q-dialog>
+
+</template>
--- src/pages/User/Milkteas/Index.vue
+++ src/pages/User/Milkteas/Index.vue
@@ -1,10 +1,135 @@
 <script setup>
 
+    import { ref, onMounted } from 'vue'
+    import ProductCard from 'src/components/User/ProductCard.vue'
+    import FeaturedProductCard from 'src/components/User/FeaturedProductCard.vue'
+
+    const slide = ref(1)
+    const categories = [
+        'Milktea',
+        'Lorem upsum',
+        'Mango',
+        'Shake',
+        'Or other things',
+    ]
+
+    const currentCategory = ref(null)
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                currentCategory.value = entry.target.id
+            }
+        })
+    }, {threshold: 0.5})
+
+    onMounted(() => {
+        const categoryElements = document.querySelectorAll('.category')
+        categoryElements.forEach(categoryElement => {
+            observer.observe(categoryElement)
+        })
+    })
+
+    function scrollToSection(section) {
+        console.log(section)
+        document.getElementById(section).scrollIntoView({ behavior: 'smooth', block: 'start' })
+    }
 
 </script>
 
 <template>
     <div>
-        <h1>Milktea page</h1>
+        <q-img
+            cover
+            src="masarap milktea.jpeg"
+            :ratio="16/9"
+            height="300px"
+            class="rounded-borders"
+        />
+        <p class="q-mt-md">
+            <span class="text-h3">RJC Cafe</span>
+            <br>
+            <span class="text-h6">San Vicente, Olango Island</span>
+        </p>
+        <q-separator/>
+        <div class="row">
+            <div class="col-3 sidebar" style="position: relative;">
+                <div style="position: sticky; top: 100px;">
+                    <q-list padding class="q-mr-xl">
+                        <q-item-label header>Categories</q-item-label>
+                        <q-item clickable v-ripple>
+                            <q-item-section>
+                                Featured items
+                            </q-item-section>
+                        </q-item>
+                        <q-item
+                            active-class="bg-secondary text-white" 
+                            :active="currentCategory == category" 
+                            clickable 
+                            @click="scrollToSection(category)" 
+                            v-ripple 
+                            v-for="category in categories" 
+                            :key="category"
+                        >
+                            <q-item-section>
+                                {{ category }}
+                            </q-item-section>
+                        </q-item>
+                    </q-list>
+                </div>
+            </div>
+            <div class="col-9">
+                <q-item>
+                    <q-item-section class="text-h6">
+                        Featured items
+                    </q-item-section>
+                    <q-item-section side>
+                        <div class="text-grey-8 q-gutter-xs">
+                            <q-btn class="gt-xs" size="12px" round icon="arrow_back" @click="slide--" />
+                            <q-btn class="gt-xs" size="12px" round icon="arrow_forward" @click="slide++" />
+                        </div>
+                    </q-item-section>
+                </q-item>
+                <div class="row">
+                    <q-carousel
+                        v-model="slide"
+                        transition-prev="slide-right"
+                        transition-next="slide-left"
+                        swipeable
+                        animated
+                        height="300px"
+                        class="full-width"
+                    >
+                        <q-carousel-slide :name="1">
+                            <div class="row fit items-center q-col-gutter-sm">
+                                <FeaturedProductCard v-for="n in 4" :key="n"/>
+                            </div>
+                        </q-carousel-slide>
+                        <q-carousel-slide :name="2">
+                            <div class="row fit items-center q-col-gutter-sm">
+                                <FeaturedProductCard v-for="n in 4" :key="n"/>
+                            </div>
+                        </q-carousel-slide>
+                    </q-carousel>
+                </div>
+                <div v-for="(category, i) in categories" :key="i" class="q-mt-md category" :id="category">
+                    <q-item class="text-h6">{{ category }}</q-item>
+                    <div class="row q-col-gutter-md">
+                        <div class="col-6" v-for="n in 5" :key="n">
+                            <ProductCard/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+
+<style scoped>
+
+    .sidebar {
+        position: sticky;
+        top: 0;
+    }
+
+</style>
